cd7d53f5866e0339c7ac26603cd09f400fea0abf
Geemmeet <ellen.gemback@gmail.com>
2025-05-12 12:40:36 +0200

    Card
    Created card outline

--- projekt-da395a/src/app/page.js
+++ projekt-da395a/src/app/page.js
@@ -1,7 +1,15 @@
 import Image from "next/image";
 import Navbar from "../components/navbar";
+import Card from "../components/card";
+
 export default function Home() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <div className="flex flex-wrap flex-row justify-center mt-10">
+        <Card />
+        <Card />
+      </div>
+    </div>
   );
 }
