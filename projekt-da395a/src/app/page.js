76b767567bf00c714a182623992ada9a05f43b0d
Geemmeet <ellen.gemback@gmail.com>
2025-05-12 12:57:38 +0200

    Reload
    Created reload component

--- projekt-da395a/src/app/page.js
+++ projekt-da395a/src/app/page.js
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import Navbar from "../components/navbar";
 import Card from "../components/card";
+import Reload from "../components/reloadbtn";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <div className="flex flex-wrap flex-row justify-center mt-10">
+      <div className="flex flex-wrap flex-row justify-center items-center mt-10">
         <Card />
+        <Reload />
         <Card />
       </div>
     </div>
---
+++ projekt-da395a/src/components/reloadbtn.jsx
@@ -0,0 +1,11 @@
+export default function reload() {
+  return (
+    <div className="w-20 bg-pink-500 rounded-full">
+      <img
+        className="w-95 p-3 h-full object-cover"
+        src="/reload.png"
+        alt="reload"
+      />
+    </div>
+  );
+}
