074d4a50bb8065517be1aab7fd47221035cf88fe
Tobias <156711407+tobi-1337@users.noreply.github.com>
2025-05-26 12:35:56 +0200

    hämtar ingredienser första gången sidan laddas

--- projekt-da395a/src/app/page.js
+++ projekt-da395a/src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Image from "next/image";
 
 //Components
@@ -17,7 +17,10 @@ export default function Home() {
       console.log(result);
       setIngredients(result);
     };
-
+    
+  useEffect(() => {
+    handleIngredients();
+  }, []);
 
   return (
     <div>
