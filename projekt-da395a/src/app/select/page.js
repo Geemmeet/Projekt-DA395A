eb790ab181b37276abe62498e7aeeb0a8b963d1c
inessuhonjic <ines.suhonjic@hotmail.se>
2025-05-28 09:25:42 +0200

    Update page.js

--- projekt-da395a/src/app/select/page.js
+++ projekt-da395a/src/app/select/page.js
@@ -7,17 +7,21 @@ import Navbar from "@/components/Navbar";
 import Reload from "@/components/Reloadbtn";
 
 import { FooterComponent } from "@/components/Footer";
+import { categorizeIngredients } from "@/lib/spoonacularUtils/categorizeIngredients";
 
-export default function SelectPage() {
+
+export default function SelectPage() { //Vad vill vi döpa  komponent till -> matcha mapp namnet!!
+  
+  
   return (
     <>
       <div> 
         <Navbar />
         <main>
           <div className="flex flex-row w-3/6 lg:flex-row  px-4 py-8 mx-auto gap-8 lg:gap-16 lg:py-16 md:flex-row-reverse">
-          <IngredientCard text="testar proppen" />
+          <IngredientCard text="här kanske vi kan ha ingridiens 1" />
           <Reload/>
-          <IngredientCard text="testar den andra proppen"/>
+          <IngredientCard text="ingrediens 2"/>
           </div>
         </main>
         <FooterComponent />
