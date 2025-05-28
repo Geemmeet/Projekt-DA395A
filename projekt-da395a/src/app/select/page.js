80480571f4a60d74cf51dbf4ac5f3cc1a848aa48
tobi-1337 <tobiasbjorklund1@gmail.com>
2025-05-28 10:27:49 +0200

    flyttade startsidan till select

--- projekt-da395a/src/app/select/page.js
+++ projekt-da395a/src/app/select/page.js
@@ -1,31 +1,51 @@
-'use client'
-//Component function
+"use client";
+import {useState, useEffect} from 'react';
+import { IngredientCard } from '@/components/IngredientCard';
 
 //Components
-import { IngredientCard } from "@/components/IngredientCard";
-import Navbar from "@/components/Navbar";
-import ReloadBtn from "@/components/ReloadBtn";
-import { FooterComponent } from "@/components/Footer";
+
+import Navbar from "../components/Navbar";
+import ReloadBtn from "../components/ReloadBtn";
+import { getIngredients } from "../lib/randomUtils/getIngredients"
+import RecipeList from '@/components/RecipeList';
+
+export default function Home() {
+
+    const [ingredients, setIngredients] = useState([]);
+    const [chosenIngredients, setChosenIngredients] = useState([]);
 
 
 
-export default function Select() { 
-  
-  
+    const handleIngredients = async () => {
+      const result = await getIngredients();
+      console.log(result);
+      setIngredients(result);
+    };
+
+    const handleChosenIngredients = (choIngr) => {
+      console.log(choIngr.name);
+      setChosenIngredients([...chosenIngredients, choIngr.name]);
+      handleIngredients();
+    }
+  //Kör handleIngredients en gång när sidan laddas
+  useEffect(() => {
+    handleIngredients();
+  }, []);
+
   return (
     <>
-      <div> 
-        <Navbar />
-        <main>
-          <div className="flex flex-row w-3/6 lg:flex-row  px-4 py-8 mx-auto gap-8 lg:gap-16 lg:py-16 md:flex-row-reverse">
-          <IngredientCard text="här kanske vi kan ha ingridiens 1" />
-          <ReloadBtn/>
-          <IngredientCard text="ingrediens 2"/>
-          </div>
-        </main>
-        <FooterComponent />
-      </div>
-
+      <div>
+      <Navbar />
+      <main>
+        <h1 className="text-center mt-10 text-2xl">Gör ditt val!</h1>
+        <div className="flex flex-wrap flex-row justify-center items-center mt-10">
+          <IngredientCard onClick={handleChosenIngredients} ingredients={ingredients[0]}/>
+          <ReloadBtn />
+          <IngredientCard onClick={handleChosenIngredients} ingredients={ingredients[1]} />
+        </div>
+        <RecipeList chosenIngredients={chosenIngredients} />
+      </main>
+    </div>
     </>
   );
 }
