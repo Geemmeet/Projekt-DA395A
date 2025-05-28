265af65ccc982f6601126ff0970f50ab3ca996f2
inessuhonjic <ines.suhonjic@hotmail.se>
2025-05-28 10:51:50 +0200

    lite styling

--- projekt-da395a/src/components/Homepage.jsx
+++ projekt-da395a/src/components/Homepage.jsx
@@ -10,20 +10,20 @@ export default function Homepage() {
 
       <div>
         <section className="bg-white dark:bg-gray-900">
-          <div className="flex flex-col lg:flex-row  px-4 py-8 mx-auto gap-8 lg:gap-16 lg:py-16 md:flex-row-reverse ">
+          <div className="flex flex-col lg:flex-col  px-4 py-8 mx-auto gap-8 lg:gap-16 lg:py-16 md:flex-col ">
           
-            <div className="order-first lg:order-last flex-shrink-0 md:self-start md:basis-2/4 lg:basis-5/12 flex justify-center lg:justify-start">
-              <img src="https://www.fmt.se/wp-content/uploads/2023/02/logo-placeholder-image.png" alt="mockup" className="max-w-xs md:max-w-sm lg:max-w-md w-full h-auto" />
+            <div className="order-first flex-shrink-0  md:basis-2/4 lg:basis-5/12 flex justify-center lg:justify-center">
+              <img src="https://www.svgrepo.com/show/66980/fast-food-placeholder.svg" alt="mockup" className="max-w-xs md:max-w-sm lg:max-w-md w-full h-auto" />
             </div>
 
 
-            <div className="self-center md:text-center lg:text-left lg:basis-8/12">
+            <div className="self-center md:text-center lg:text-center lg:basis-8/12">
               <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Your receipe inspiration place</h1>
               <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Choose your diet and your set to go</p>
 
-              <div className="flex flex-col md:flex-row">
+              <div className="flex flex-col md:flex-row justify-center">
                 
-                  <Select id="countries" className="w-full me-1 md:w-1/2" defaultValue="none">
+                  <Select id="countries" className="w-full me-1 md:w-1/2 justify-center" defaultValue="none">
                     <option value="none" disabled hidden>Choose your diet..</option>
                     <option value="vegetarian">Vegetarian</option>
                     <option value="pescetarian">Pescetarian</option>
