d7bb25d979354f63df544f42610f021cbacd6a4a
Geemmeet <ellen.gemback@gmail.com>
2025-06-02 10:34:26 +0200

    Upd
    Design upd

--- projekt-da395a/src/components/IngredientCard.jsx
+++ projekt-da395a/src/components/IngredientCard.jsx
@@ -9,11 +9,9 @@ export default function IngredientCard({ ingredients = {name: "apricots", image 
     <Card
       onClick={onClick}
       className="max-w-sm w-1/2"
-      imgAlt="Meaningful alt text for an image that is not purely decorative"
-      imgSrc={"https://img.spoonacular.com/ingredients_100x100/" + ingredients.image}
       >
 
-      <h5 className="capitalize text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:cursor-pointer">
+      <h5 className="capitalize text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:cursor-pointer text-center">
         {ingredients.name}
       </h5>
     </Card>
