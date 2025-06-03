518dec8da30062eaab2f20e4fba7ca916f3a03ca
inessuhonjic <ines.suhonjic@hotmail.se>
2025-06-03 15:06:34 +0200

    fixing

--- projekt-da395a/src/components/ShowSavedRecipes/DesignOfSavedRecipe.jsx
+++ projekt-da395a/src/components/ShowSavedRecipes/DesignOfSavedRecipe.jsx
@@ -27,7 +27,7 @@ export default function DesignOfSavedRecipe ({item, index}) {
                 className = "mt-2 self-end focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                 onClick={(e) => {
                 e.stopPropagation();
-                removeRecipe(item.id)}}> 
+                removeRecipe(item.recipeId)}}> 
                 
                 <svg className="w-3 h-3 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18 17.94 6M18 18 6.06 6" />
--- projekt-da395a/src/lib/localstorageFunctionality/savedRecipesContext.js
+++ projekt-da395a/src/lib/localstorageFunctionality/savedRecipesContext.js
@@ -22,10 +22,9 @@ export function SavedRecipesProvider({ children }) {
     //https://www.w3schools.com/jsref/jsref_some.asp
     //Check if the recipe is already saved
     function saveRecipe(newRecipe) {
-        console.log(newRecipe)
-        if (savedRecipes.some(recipe => recipe.id === newRecipe.recipeId)) {
-            console.log("hej!")
-            console.log(recipe)
+        
+        if (savedRecipes.some(recipe => recipe.recipeId === newRecipe.recipeId)) {
+            
             alert('Recipe already saved!');
             return;
         }
@@ -45,7 +44,7 @@ export function SavedRecipesProvider({ children }) {
 
     function removeRecipe(recipeId) {
         //filter out the deleted recipe
-        const updated = savedRecipes.filter(recipe => recipe.id !== recipeId)
+        const updated = savedRecipes.filter(recipe => recipe.recipeId !== recipeId)
 
         //save the updated array to useState
         setSavedRecipes(updated);
