8c75ead71595088b0058b274f3532b9c3d26194a
inessuhonjic <ines.suhonjic@hotmail.se>
2025-06-05 10:35:28 +0200

    layout

--- projekt-da395a/src/components/Homepage/Homepage.jsx
+++ projekt-da395a/src/components/Homepage/Homepage.jsx
@@ -16,16 +16,16 @@ export default function Homepage() {
       <div className="homepage">
         <section className="bg-white dark:bg-gray-900">
           <div className="flex flex-col lg:flex-col  px-4 py-8 mx-auto gap-8 lg:gap-16 lg:py-16 md:flex-col ">
-            <div className="order-first flex-shrink-0  md:basis-2/4 lg:basis-5/12 flex justify-center lg:justify-center">
+            <div className="order-first mb-[-4rem] flex-shrink-0  md:basis-2/4 lg:basis-5/12 flex justify-center lg:justify-center">
               <img
                 src="/fast-food-placeholder.svg"
                 alt="mockup"
                 className="sm:max-w-xs mt-0 mb-0 md:max-w-md lg:max-w-md w-1/8 h-auto"
               />
             </div>
-            <div className="self-center md:text-center lg:text-center lg:basis-8/12">
-              <h1 className= "text-4xl sm:text-6xl lg:text-8xl mb-4 max-w-xl">RECIPE <br></br> inspiration place</h1>
-              <p className="fraunces max-w-2xl mb-6 font-light text-gray-900 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Choose your diet and you're set to go</p>
+            <div className="self-center mt-0 md:text-center lg:text-center lg:basis-8/12">
+              <h1 className= "text-4xl sm:text-6xl lg:text-8xl mb-4 max-w-xl">a recipe <br></br> inspiration place</h1>
+              <p className="fraunces max-w-2xl mb-6 font-light text-gray-900 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Choose your diet and your set to go</p>
 
               <div className="flex flex-col md:flex-row justify-center">
                 <Select
@@ -46,7 +46,7 @@ export default function Homepage() {
                 </Select>
                 <div>
                   <Button
-                    className="mt-3 lg:mt-0 lg:ms-3 bg-white text-gray-800 border border-gray-300 hover:bg-gray-500 hover:text-white hover:cursor-pointer"
+                    className="mt-3 lg:mt-0 lg:ms-3 bg-white text-gray-800 border border-gray-300 hover:bg-[#DAD5F3] hover:text-white hover:cursor-pointer"
                     onClick={() => router.push("/select/" + diet)}
                   >
                     {/* push() in this context is a router function NOT a funcition to add to an array. */}
--- projekt-da395a/src/components/Select/ModalComponent.jsx
+++ projekt-da395a/src/components/Select/ModalComponent.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { Popover } from "flowbite-react";
 
 import {
   Button,
@@ -11,18 +12,31 @@ import { useRouter } from "next/navigation";
 
 export function ModalComponent({ show, onClose, recipe }) {
   const router = useRouter();
+  const content = (<div className="w-64 text-sm text-gray-500 dark:text-gray-400">
+    <div className="border-b border-gray-200 bg-gray-100 px-3 py-2 dark:border-gray-600 dark:bg-gray-700">
+      <h3 className="londrina_solid font-semibold text-gray-900 dark:text-white">Price Range</h3>
+    </div>
+    <div className="px-3 py-2">
+      <p className="text-red-400"> $$$ = Expensive </p>
+      <p className="text-amber-400">$$ = Medium</p>
+      <p className="text-green-400">$ = Cheap</p>
+    </div>
+  </div>)
 
-  function pricePerServing (pricePerS) {
+  function pricePerServing(pricePerS) {
     const price = parseFloat(pricePerS)
-    if (price <= 200 ) {
-      return ("$")
+    if (price <= 200) {
+      return (<p className="text-green-400 fraunces text-base leading-relaxed mr-2">$</p>)
     }
-    else if (price <= 400 ){
-      return ("$$")
+    else if (price <= 400) {
+      return (<p className="text-amber-400 fraunces text-base leading-relaxed mr-2">$$</p>)
     }
     else {
-      return ("$$$")
+      return (<p className="text-red-400 fraunces text-base leading-relaxed mr-2"> $$$</p>)
     }
+
+
+
   }
   return (
     <>
@@ -33,7 +47,7 @@ export function ModalComponent({ show, onClose, recipe }) {
             <img
               src={recipe.image}
               alt={recipe.title}
-              className="w-full h-40 object-cover rounded-md mb-2 rounded-xl shadow-lg"
+              className="w-full h-40 object-cover rounded-md mb-2 shadow-lg"
             />
             <p className="fraunces text-base leading-relaxed text-gray-500 dark:text-gray-400 mr-5 inline">
               Cookingtime: {recipe.readyInMinutes}
@@ -41,9 +55,20 @@ export function ModalComponent({ show, onClose, recipe }) {
             <p className="fraunces text-base leading-relaxed text-gray-500 dark:text-gray-400 mr-5 inline">
               Servings: {recipe.servings}
             </p>
-            <p className="fraunces text-base leading-relaxed text-gray-500 dark:text-gray-400 mr-5 inline">
-              Pricerange: {pricePerServing(recipe.pricePerServing)}
-            </p>
+
+            <Popover content={content} trigger="hover" placement="top">
+              <span className="inline-flex items-center hover:cursor-pointer">
+              
+                Pricerange: {pricePerServing(recipe.pricePerServing)}
+             
+              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+              </span>
+
+            </Popover>
+
+
             <div className="fraunces" dangerouslySetInnerHTML={{ __html: recipe.summary }} />
           </div>
           {/*The Api returns a summary text with HTML tags. Solution: https://www.geeksforgeeks.org/reactjs-dangerouslysetinnerhtml-attribute*/}
